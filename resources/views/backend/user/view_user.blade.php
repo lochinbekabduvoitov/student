68e63d1185652d3204784c6199e9afcf5cf262da
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-01-01 22:44:21 +0500

    user view page creadet

---
+++ resources/views/backend/user/view_user.blade.php
@@ -0,0 +1,87 @@
+@extends('admin.admin_master')
+
+@section('admin')
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <div class="container-full">
+    
+
+      <!-- Main content -->
+      <section class="content">
+        <div class="row">
+
+
+
+          <div class="col-12">
+
+           <div class="box">
+              <div class="box-header with-border">
+                <h3 class="box-title">Data Table With Full Features</h3>
+              </div>
+              <!-- /.box-header -->
+              <div class="box-body">
+                  <div class="table-responsive">
+                    <table id="example1" class="table table-bordered table-striped">
+                      <thead>
+                          <tr>
+                              <th>Name</th>
+                              <th>Position</th>
+                              <th>Office</th>
+                              <th>Age</th>
+                              <th>Start date</th>
+                              <th>Salary</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                          <tr>
+                              <td>Tiger Nixon</td>
+                              <td>System Architect</td>
+                              <td>Edinburgh</td>
+                              <td>61</td>
+                              <td>2011/04/25</td>
+                              <td>$320,800</td>
+                          </tr>
+                          <tr>
+                              <td>Garrett Winters</td>
+                              <td>Accountant</td>
+                              <td>Tokyo</td>
+                              <td>63</td>
+                              <td>2011/07/25</td>
+                              <td>$170,750</td>
+                          </tr>
+
+
+
+                      </tbody>
+                      <tfoot>
+                          <tr>
+                              <th>Name</th>
+                              <th>Position</th>
+                              <th>Office</th>
+                              <th>Age</th>
+                              <th>Start date</th>
+                              <th>Salary</th>
+                          </tr>
+                      </tfoot>
+                    </table>
+                  </div>
+              </div>
+              <!-- /.box-body -->
+            </div>
+            <!-- /.box -->
+
+
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </section>
+      <!-- /.content -->
+
+    </div>
+</div>
+<!-- /.content-wrapper -->
+
+
+@endsection
